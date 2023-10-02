2e816b08b2ee598ed946241dea95f744a01549fb
DawoodKhatri <dawoodkhatri18@gmail.com>
2023-10-02 17:04:43 +0530

    added cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import authRoutes from "./routes/auth";
 import companyRoutes from "./routes/company";
 
@@ -8,6 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(cors({ origin: "*" }));
 app.use(express.static("public"));
 
 app.use("/api", authRoutes);
