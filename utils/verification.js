801d2ad0d6f7832714ca43b3e574d1fe33d462c8
DawoodKhatri <dawoodkhatri18@gmail.com>
2023-11-01 13:48:06 +0530

    Added Mailjet

--- utils/verification.js
+++ utils/verification.js
@@ -1,43 +1,34 @@
-import nodemailer from "nodemailer";
-import handlebars from "handlebars";
-import fs from "fs";
-import { fileURLToPath } from "url";
-import path from "path";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import Mailjet from "node-mailjet";
 
 export const generateOTP = () => {
   return Math.floor(100000 + Math.random() * 900000);
 };
 
 export const sendVerificationMail = async (email, otp) => {
-  const from = "CargoLinker";
-  const user = process.env.EMAIL_USERNAME;
-  const pass = process.env.EMAIL_PASSWORD;
+  try {
+    const name = email.split("@")[0];
+    
+    const mailjet = Mailjet.apiConnect(
+      process.env.MJ_API_KEY,
+      process.env.MJ_API_SECRET
+    );
 
-  const date = new Date();
-  date.setHours(date.getHours() + 1);
-
-  const htmlTemplate = fs.readFileSync(
-    path.join(__dirname, "../templates/mailTemplate.html"),
-    { encoding: "utf-8" }
-  );
-
-  const template = handlebars.compile(htmlTemplate);
-  const html = template({ name: email.split("@")[0], otp: otp, validity: 5 , api_url: process.env.API_URL});
-
-  const options = {
-    from: `${from} <${user}>`,
-    to: email,
-    subject: "Email Verification",
-    html: html,
-  };
-
-  let transpoter = nodemailer.createTransport({
-    service: "Gmail",
-    auth: { user, pass },
-  });
-
-  await transpoter.sendMail(options);
+    await mailjet.post("send", { version: "v3.1" }).request({
+      Messages: [
+        {
+          From: { Email: process.env.MJ_SENDER_EMAIL, Name: "CargoLinker" },
+          To: [{ Email: email, Name: name }],
+          TemplateID: 5233671,
+          TemplateLanguage: true,
+          Subject: "Verify your email address to create your account",
+          Variables: {
+            name: name,
+            otp: otp,
+          },
+        },
+      ],
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
