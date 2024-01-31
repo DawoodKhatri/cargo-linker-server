06afa11b325687169f297af5ed17316b2ea7c24e
Dawood Khatri <dawoodkhatri18@gmail.com>
2024-02-01 01:24:03 +0530

    Created Admin Company Verification Apis

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,6 +1,8 @@
 /** @type {import("express").RequestHandler} */
 
+import { VERIFICATION_STATUS } from "../constants/verificationStatus.js";
 import Admin from "../models/admin.js";
+import Company from "../models/company.js";
 import { errorResponse, successResponse } from "../utils/response.js";
 
 export const adminLogin = async (req, res) => {
@@ -36,6 +38,83 @@ export const adminLogin = async (req, res) => {
     return successResponse({
       res: res.cookie("token", token, options),
       message: "Login Successfull",
+    });
+  } catch (error) {
+    return errorResponse({ res, message: error.message });
+  }
+};
+
+export const getPendingVerificationCompanies = async (req, res) => {
+  try {
+    const companies = await Company.find({
+      "verification.status": VERIFICATION_STATUS.underVerification,
+    }).select("name email establishmentDate serviceType");
+
+    return successResponse({
+      res,
+      message: "Pending Verifications",
+      data: { companies },
+    });
+  } catch (error) {
+    return errorResponse({ res, message: error.message });
+  }
+};
+
+export const getCompanyDetails = async (req, res) => {
+  try {
+    const { companyId } = req.params;
+    const company = await Company.findById(companyId);
+    if (!company)
+      return errorResponse({ res, status: 404, message: "Company not found" });
+
+    return successResponse({
+      res,
+      message: "Company Details",
+      data: { company },
+    });
+  } catch (error) {
+    return errorResponse({ res, message: error.message });
+  }
+};
+
+export const acceptCompany = async (req, res) => {
+  try {
+    const { companyId } = req.params;
+
+    const company = await Company.findById(companyId);
+    if (!company)
+      return errorResponse({ res, status: 404, message: "Company not found" });
+
+    company.verification.status = VERIFICATION_STATUS.verified;
+    await company.save();
+
+    return successResponse({
+      res,
+      message: "Company Accepted",
+    });
+  } catch (error) {
+    return errorResponse({ res, message: error.message });
+  }
+};
+
+export const rejectCompany = async (req, res) => {
+  try {
+    const { companyId } = req.params;
+    const { remark } = req.body;
+    if (!remark)
+      return errorResponse({ res, status: 400, message: "Remark Required" });
+
+    const company = await Company.findById(companyId);
+    if (!company)
+      return errorResponse({ res, status: 404, message: "Company not found" });
+
+    company.verification.status = VERIFICATION_STATUS.rejected;
+    company.verification.remark = remark;
+    await company.save();
+
+    return successResponse({
+      res,
+      message: "Company Rejected",
     });
   } catch (error) {
     return errorResponse({ res, message: error.message });
--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,42 @@
 import { Router } from "express";
-import { adminLogin } from "../controllers/admin.js";
+import {
+  acceptCompany,
+  adminLogin,
+  getCompanyDetails,
+  getPendingVerificationCompanies,
+  rejectCompany,
+} from "../controllers/admin.js";
+import { isAuthenticated, isAdmin } from "../middlewares/checkAuth.js";
 
 const adminRoutes = Router();
 
 adminRoutes.post("/login", adminLogin);
+adminRoutes.get(
+  "/pendingVerificationCompanies",
+  isAuthenticated,
+  isAdmin,
+  getPendingVerificationCompanies
+);
+
+adminRoutes.get(
+  "/company/:companyId",
+  isAuthenticated,
+  isAdmin,
+  getCompanyDetails
+);
+
+adminRoutes.post(
+  "/company/:companyId/accept",
+  isAuthenticated,
+  isAdmin,
+  acceptCompany
+);
+
+adminRoutes.post(
+  "/company/:companyId/reject",
+  isAuthenticated,
+  isAdmin,
+  rejectCompany
+);
 
 export default adminRoutes;
